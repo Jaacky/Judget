73c27635fe7734b54df970b19da5169f427ffffe
Jacky <jaackyvan@gmail.com>
2015-12-28 22:33:52 -0500

    Adding default value for option attribute in Purchase model

--- models/Purchase.js
+++ models/Purchase.js
@@ -5,7 +5,7 @@ var purchaseSchema = new Schema({
 	name : { type: String, required: true },
 	dateCreated : { type: Date, required: true, default: Date.now },
 	price : { type: Number, required: true },
-	option : { type: String, enum: ['useful', 'extra', 'investment'] }
+	option : { type: String, enum: ['useful', 'extra', 'investment'], default: 'useful' }
 });
 
 module.exports = mongoose.model('Purchase', purchaseSchema);
